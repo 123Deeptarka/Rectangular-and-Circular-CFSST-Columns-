831c7b06e09a54b5854b617f0d6c7ef826b0356d
Deeptarka Roy <98172591+123Deeptarka@users.noreply.github.com>
2024-02-05 19:33:31 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 19:20:48 2024
+
+@author: deeptarkaroy
+"""
+
+import streamlit as st
+import pickle 
+from streamlit_option_menu import option_menu
+
+
+
+
+
+with st.sidebar:
+     
+    selected=option_menu("Axial Capacity Prediction System",
+                         ["Circular CFSST Columns",
+                          "Rectangular CFSST Columns"],default_index=0)
